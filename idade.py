8b0ba1f8184caba25fd9216a376cb95154abd66c
Pedro Salles <pedro.silva15@alunos.unis.edu.br>
2021-10-31 21:21:40 -0300

    Adicionado_os_comentarios

--- idade.py
+++ idade.py
@@ -1,12 +1,16 @@
+"""Faça um programa que leia a idade de uma pessoa expressa
+em dia,mes e ano e  mostra em anos meses e dia."""
+
+
 from datetime import date
 data = date.today()
 data_atual = '{}/{}/{}'.format(data.day, data.month, data.year)
 dia = int(input('digite o dia do seu nascimento: '))
 mes = int(input('Digite do mes do seu nascimento: '))
 ano = int(input('Digite o ano do seu nascimento: '))
-sua_idade = data.year - ano
-meses = sua_idade * 12
-dias =  sua_idade * 365
+sua_idade = data.year - ano #CALCULA OS ANOS#
+meses = sua_idade * 12 #CALCULA OS MESES#
+dias =  sua_idade * 365 #CALCULA OS DIAS#
 print('Você tem {} anos, {} meses e {} dias '.format(sua_idade, meses, dias))
 
 print('Consulta realizada na data ', data_atual)
