6455a6bd5d5e4808fa29244685e70cc29d2c3d3e
Pedro Salles <pedro.silva15@alunos.unis.edu.br>
2021-11-02 15:37:18 -0300

    Numero_Primo

---
+++ numero_primo.py
@@ -0,0 +1,15 @@
+# Testa se o número é primo
+def numprimo(n):
+
+    if n < 2:
+        return False
+
+    for i in range(2, n):
+        if not n % i:
+            return False
+    else:
+        return True
+
+for x in range(1, 101):###Vai de 1 ate 100###
+    if numprimo(x):
+        print (x)
