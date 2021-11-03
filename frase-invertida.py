12d248d45af6629613a508ef0f3959b8c0d23864
Pedro Salles <pedro.silva15@alunos.unis.edu.br>
2021-11-03 18:48:01 -0300

    Inverter_uma_frase

---
+++ frase-invertida.py
@@ -0,0 +1,8 @@
+'''Escreva uma função que: a) Receba uma frase como parâmetro.
+b) Retorne uma nova frase com cada palavra com as letras invertidas.'''
+###DECLARAÇÃO DA FUNÇÃO PARA INVERTER A FRASE
+def inverter(texto):
+    return texto[::-1]
+
+print('Essa frase vai ser invertida')
+print(inverter('Essa frase vai ser invertida'))###CHAMA A FUNÇÃO INVERTER E INVERTE A FRASE
